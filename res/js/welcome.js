1561e22ccede3e9b258f69688a2891c271d34152
navilindo <hassainjr@gmail.com>
2023-02-22 23:57:00 +0200

    js

---
+++ res/js/welcome.js
@@ -0,0 +1,13 @@
+var text = ["Welcome Friend!", "Karibu Rafiki!", "!Ù…Ø±Ø­Ø¨Ø§ ØµØ¯ÙŠÙ‚Ù‰"];
+var counter = 0;
+var elem = document.getElementById("welcomeText");
+var inst = setInterval(change, 2000);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
