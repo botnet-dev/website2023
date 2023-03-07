131796b17bfaf9f0b95194f74903f6cbf79b2584
navilindo <hassainjr@gmail.com>
2023-03-07 03:52:59 +0200

    final

--- res/js/welcomeText.js
+++ res/js/welcomeText.js
@@ -1,26 +1,28 @@
 var text = [
 
-    '<i class="bi bi-app-indicator carousel-icon"></i>' +
+    '<br> <br> <i class="bi bi-app-indicator carousel-icon"></i>' +
     '<h2>' +
         '<b>Technology for Innovators</b>' +
     '</h2>' +
-    '<p>The future belongs to those who evolve.</p>',
+    '<p>The future belongs to those who evolve. <br><br></p>',
 
-    '<i class="bi bi-shield-check carousel-icon"></i>' +
+    '<br> <br> <i class="bi bi-shield-check carousel-icon"></i>' +
     '<h2>' +
         '<b>We provide Security Solutions</b>' +
     '</h2>' +
-    '<p>CCTV cameras Installation, Cyber security and Fire alarm installation.</p>',
+    '<p>CCTV cameras Installation, Cyber security and Fire alarm installation. <br><br></p>',
 
-    '<i class="bi bi-boxes carousel-icon"></i>' +
+    '<br><br> <i class="bi bi-boxes carousel-icon"></i>' +
     '<h2>' +
         '<b>Technology Experts</b>' +
     '</h2>' +
-    '<p>We provide technology solutions both in software development and networking.</p>'
+    '<p>We provide technology solutions both in software development <br> and networking.</p>',
+
+    '<br><br><br><br><br> click <i class="bi bi-arrow-down-short"></i> to visit our website'
   
 ];
 
-var botLogo = '<br> <a href="home.html" class="arrowBegin pt-5">' +
+var botLogo = '<br><br> <a href="home.html" class="arrowBegin pt-5">' +
                 
                 '<img src="./res/images/logos/logo-white.png" alt="botnet-logo" class="botnet-index">' +
                 
@@ -28,7 +30,7 @@ var botLogo = '<br> <a href="home.html" class="arrowBegin pt-5">' +
 
 var counter = 0;
 var elem = document.getElementById("welcomeText");
-var inst = setInterval(change, 3000);
+var inst = setInterval(change, 2000);
 
 function change() {
   elem.innerHTML = text[counter] + botLogo;
